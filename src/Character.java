4210e6b847975d46e389583f7e91f2bd47c97b90
yamamotonatuki <241058`@std.hi-joho.ac.jp>
2025-06-23 11:08:16 +0900

    友達が修正してくれてたから分からなくなちゃった

--- src/Character.java
+++ src/Character.java
@@ -2,6 +2,11 @@ public abstract class Character implements Creature {
     String name;
     int hp;
 
+    public Character() {
+        this.name = name;
+        this.hp = hp;
+    }
+
     public final boolean isAlive() {
         return hp > 0;
     }
